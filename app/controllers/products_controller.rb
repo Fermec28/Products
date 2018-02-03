f469ee698fa79509603e080682fa6e8c5c7f5222
Jose Medina <fermed28@gmail.com>
2018-02-03 17:10:45 -0500

    CRD of CRUD done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,35 @@
 class ProductsController < ApplicationController
-	def show
-		
+	def index
+		@products= Product.all		
+	end
+
+	def new
+		@product = 	Product.new
+	end
+
+	def edit
+		@product = Product.find(params[:id])
+	end
+
+	def destroy
+		product = Product.find(params[:id])
+		product.destroy
+		redirect_to products_path
+	end
+
+	def create
+		product = Product.new(product_params)
+		if product.save
+			redirect_to products_path
+		else
+		end
+	end
+
+
+	private
+
+	def product_params
+		params.require(:product).permit(:name,:price,category_ids:[])
 	end
 
 end
